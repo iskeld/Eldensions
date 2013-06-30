e649386e63452d9d32cda324df3af3177dd3c033
Maciej Woźniak <iskeld@gmail.com>
2013-06-30 21:26:17 +0200

    Initial commit

---
+++ Eldensions/Extensions/CollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace EldSharp.Eldensions.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static bool HasDuplicates<TItem>(this ICollection<TItem> list)
+        {
+            return list.Count != list.Distinct().Count();
+        }
+    }
+}
---
+++ Eldensions/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace EldSharp.Eldensions.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static bool HasDuplicates<TItem>(this IEnumerable<TItem> list)
+        {
+            return list.ToList().HasDuplicates();
+        }
+
+        public static IEnumerable<TItem> GetDuplicatedItems<TItem>(this IEnumerable<TItem> list)
+        {
+            HashSet<TItem> set = new HashSet<TItem>();
+            return list.Where(item => !set.Add(item));
+        }
+    }
+}
